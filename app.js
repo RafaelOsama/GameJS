c647a88b4ec3d32581adc3aa443988207ee7f4ce
Rafael Amaral <rafaelosama80@gmail.com>
2023-10-02 11:38:35 -0300

    enquanto

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 7;
 console.log(numeroSecreto)
-let  chute = prompt('Escolha um número entre 1 e 0');
+let chute
 
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto}`);
-}else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
-    }else {
-        alert(`O número secreto é maior que ${chute}`);
+//enquanto chute não for igual
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 0');
+    //se chute for igual ao numero secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto}`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
     }
 }
+
