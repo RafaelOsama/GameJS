955e7747ec1d154d66ace65dfdc8ac86e59ba19d
Rafael Amaral <rafaelosama80@gmail.com>
2023-10-02 11:23:48 -0300

    novas condições

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ let  chute = prompt('Escolha um número entre 1 e 0');
 if (chute == numeroSecreto) {
     alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto}`);
 }else {
-    alert('Você errou :(')
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    }else {
+        alert(`O número secreto é maior que ${chute}`);
+    }
 }
