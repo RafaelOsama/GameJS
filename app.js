ba99afb8098772a590e491c4a25254c41d3ff09a
Rafael Amaral <rafaelosama80@gmail.com>
2023-10-02 11:07:11 -0300

    Concatenação

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 7;
 console.log(numeroSecreto)
 let  chute = prompt('Escolha um número entre 1 e 0');
 
 if (chute == numeroSecreto) {
-    alert('Isso aí! Você descobriu o nùmero secreto (5)');
+    alert(`Isso aí! Você descobriu o nùmero secreto ${numeroSecreto}`);
 }else {
     alert('Você errou :(')
 }
