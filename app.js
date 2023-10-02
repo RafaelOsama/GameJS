2768e4146f0980c74743ba38ae3231b05bcb52eb
Rafael Amaral <rafaelosama80@gmail.com>
2023-10-02 10:34:53 -0300

    comparando variáveis

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-prompt('Escolha um número entre 1 e 0');
+let  chute = prompt('Escolha um número entre 1 e 0');
+
+if (chute == numeroSecreto) {
+    console.log('Isso aí! Você descobriu o nùmero secreto (5)');
+}
